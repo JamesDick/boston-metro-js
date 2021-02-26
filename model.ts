120b1b6b3e0e6891f70b2cfdd5620b773dd223fc
James Dick <slightlyjames@gmail.com>
2021-02-26 00:20:02 +0000

    Implemented the graph generation with a limited set of nodes and edges since it's not being parsed from the file since I can't be bothered implementing a parser right now.

--- model.ts
+++ model.ts
@@ -78,13 +78,13 @@ class Multigraph extends Graph {
         let expansions: number = 0;
 
         while (agenda != []) {
-            let current = agenda.pop();
+            let current: number = agenda.pop();
             if (current == dest) {
                 return this.reconstructPath(current, parents).map(s => this._vertices.get(s));
             }
 
             if (expansions++ < 50000) {
-                let adjacent = this.adjacentStations(current);
+                let adjacent: number[] = this.adjacentStations(current);
                 for (let a of adjacent) {
                     if (!parents.has(a) && a != src) {
                         agenda.push(a);
@@ -98,7 +98,7 @@ class Multigraph extends Graph {
     }
 
     private reconstructPath(current: number, parents: Map<number, number>): number[] {
-        let path = [current];
+        let path: number[] = [current];
         while (parents.has(current)) {
             current = parents.get(current);
             path = [current, ...path];
@@ -107,15 +107,11 @@ class Multigraph extends Graph {
     }
 
     private adjacentStations(id: number): number[] {
-        let adjacentStations = [];
+        let adjacentStations: number[] = [];
 
         for (let e of this._edges) {
             if (e.src == id) {
                 adjacentStations.push(e.dest);
-            }
-
-            if (e.dest == id) {
-                adjacentStations.push(e.src);
             }
         }
 
@@ -138,7 +134,68 @@ class Model implements IModel {
     private generateGraphFromFile(): Multigraph {
         let multigraph = new Multigraph();
         
-        /* do stuff */
+        let stations = [
+            {id: 1, name: 'Oak Grove'},
+            {id: 2, name: 'Malden'},
+            {id: 3, name: 'Wonderland'},
+            {id: 4, name: 'Revere Beach'},
+            {id: 5, name: 'Wellington'},
+            {id: 6, name: 'Beachmont'},
+            {id: 7, name: 'Davis'},
+            {id: 8, name: 'Alewife'},
+            {id: 9, name: 'Suffolk Downs'},
+            {id: 10, name: 'Porter'},
+            {id: 11, name: 'Orient Heights'},
+            {id: 12, name: 'Sullivan Square'},
+            {id: 13, name: 'Wood Island'},
+            {id: 14, name: 'Harvard'},
+            {id: 15, name: 'Community College'},
+            {id: 16, name: 'Airport'},
+            {id: 17, name: 'Lechmere'},
+            {id: 18, name: 'Maverick'},
+            {id: 19, name: 'Science Park'},
+            {id: 20, name: 'North Station'}
+        ];
+
+        let rails = [
+            {src: 1, dest: 2, line: 'Orange'},
+            {src: 2, dest: 1, line: 'Orange'},
+            {src: 2, dest: 5, line: 'Orange'},
+            {src: 3, dest: 4, line: 'Blue'},
+            {src: 4, dest: 3, line: 'Blue'},
+            {src: 4, dest: 6, line: 'Blue'},
+            {src: 5, dest: 2, line: 'Orange'},
+            {src: 5, dest: 12, line: 'Orange'},
+            {src: 6, dest: 4, line: 'Blue'},
+            {src: 6, dest: 9, line: 'Blue'},
+            {src: 7, dest: 8, line: 'Red'},
+            {src: 7, dest: 10, line: 'Red'},
+            {src: 8, dest: 7, line: 'Red'},
+            {src: 9, dest: 6, line: 'Blue'},
+            {src: 9, dest: 11, line: 'Blue'},
+            {src: 10, dest: 7, line: 'Red'},
+            {src: 10, dest: 14, line: 'Red'},
+            {src: 11, dest: 9, line: 'Blue'},
+            {src: 11, dest: 13, line: 'Blue'},
+            {src: 12, dest: 5, line: 'Orange'},
+            {src: 12, dest: 15, line: 'Orange'},
+            {src: 13, dest: 11, line: 'Blue'},
+            {src: 13, dest: 16, line: 'Blue'},
+            {src: 14, dest: 10, line: 'Red'},
+            {src: 15, dest: 12, line: 'Orange'},
+            {src: 15, dest: 20, line: 'Orange'},
+            {src: 16, dest: 13, line: 'Blue'},
+            {src: 16, dest: 18, line: 'Blue'},
+            {src: 17, dest: 19, line: 'Green'},
+            {src: 18, dest: 16, line: 'Blue'},
+            {src: 19, dest: 17, line: 'Green'},
+            {src: 19, dest: 20, line: 'Green'},
+            {src: 20, dest: 19, line: 'Green'},
+            {src: 20, dest: 15, line: 'Orange'}
+        ];
+
+        stations.forEach(s => multigraph.addVertex(new Station(s.id, s.name)));
+        rails.forEach(r => multigraph.addEdge(new Rail(r.src, r.dest, r.line)));
 
         return multigraph;
     }
