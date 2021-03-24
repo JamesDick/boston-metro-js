e942a9cbec4e1d8950777b44799082d0ac5ec94b
James Dick <slightlyjames@gmail.com>
2021-03-24 00:52:30 +0000

    Amended some naming in the Model and added documentation to the classes within.

--- controller.js
+++
@@ -1,9 +0,0 @@
-let model = new Model();
-let view = new View(model.getStations());
-view.setFindRouteBtnHandler(() => {
-    let src = view.selectedSrcId(),
-        dest = view.selectedDestId(),
-        route = model.findRoute(src, dest);
-
-    view.displayRoute(route);
-});
